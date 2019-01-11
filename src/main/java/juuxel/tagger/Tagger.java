d65b7bbe3425228255b872c28fc9cd556c2f5e1f
Juuxel <kasperi.kauppi@gmail.com>
2019-01-11 19:19:42 +0200

    Add documentation

--- src/main/java/juuxel/tagger/Tagger.java
+++ src/main/java/juuxel/tagger/Tagger.java
@@ -12,9 +12,24 @@ import net.minecraft.tag.Tag;
 import net.minecraft.util.Identifier;
 
 public class Tagger implements ModInitializer {
+    /**
+     * Deals fire damage to the player.
+     */
     public static final Tag<Block> FIRE = TagRegistry.block(new Identifier("tagger", "fire"));
+
+    /**
+     * Can be punched to destroy (for blocks without a bounding box, doesn't drop items).
+     */
     public static final Tag<Block> EXTINGUISHES_LIKE_FIRE = TagRegistry.block(new Identifier("tagger", "extinguishes_like_fire"));
+
+    /**
+     * Torches can be placed on top.
+     */
     public static final Tag<Block> CAN_SUPPORT_TORCHES = TagRegistry.block(new Identifier("tagger", "can_support_torches"));
+
+    /**
+     * Drops from grass like wheat seeds in vanilla.
+     */
     public static final Tag<Item> SEEDS = TagRegistry.item(new Identifier("tagger", "seeds"));
 
     @Override
